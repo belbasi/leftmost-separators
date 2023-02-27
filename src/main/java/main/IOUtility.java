941a2c68ccb196918e7ee8b9734257d0d3cde54f
belbasi <126216993+belbasi@users.noreply.github.com>
2023-02-26 23:40:57 -0500

    end on night commit

--- src/main/java/main/IOUtility.java
+++ src/main/java/main/IOUtility.java
@@ -1,15 +1,39 @@
 package main;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import main.graph.Edge.Edge;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class IOUtility {
 
-    public static void readFromFile(String path) {
+    public static short readFromFile(String path) {
+        final JSONParser jsonParser = new JSONParser();
         try {
-            File input = new File(path);
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find the file in " +path);
+            final JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(path));
+            final int numOfVertices = (int) jsonObject.get("numberOfVertices");
+            final Optional<JSONArray> edges = (Optional<JSONArray>) jsonObject.get("edges");
+            final Optional<JSONObject> adjacencyMatrix = (Optional<JSONObject>) jsonObject.get("adjacencyMatrix");
+            if (edges.isPresent()) {
+                Set<Edge> graphEdges = new HashSet<>();
+                edges.get().forEach(edge -> {
+                    System.out.println(edge);
+                });
+            }
+            if (adjacencyMatrix.isPresent()) {
+
+            }
+            throw new RuntimeException("Neither edges nor adjacency matrix was given as input!");
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
+        return 0;
     }
 }
---
+++ src/test/java/IOTest/IOUTilityTest.java
@@ -0,0 +1,15 @@
+package IOTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static main.IOUtility.readFromFile;
+
+public class IOUTilityTest {
+
+    @Test
+    public void testReadFromFileGivenEdges() {
+        final String path = "C:\\Users\\mahdi\\OneDrive\\Documents\\GitHub\\tree-decomposition\\src\\main\\resources\\configuration\\example-graph-with-edges.json";
+        Assertions.assertEquals(1, readFromFile(path));
+    }
+}
