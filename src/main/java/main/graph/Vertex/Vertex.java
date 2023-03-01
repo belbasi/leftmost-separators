3ff0b83cc3b211660d90d2117d38d347d581f95f
belbasi <126216993+belbasi@users.noreply.github.com>
2023-02-28 19:40:26 -0500

    Add weight to vertex

--- src/main/java/main/graph/Vertex/Vertex.java
+++ src/main/java/main/graph/Vertex/Vertex.java
@@ -2,14 +2,17 @@ package main.graph.Vertex;
 
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Vertex {
     private final int id;
+    private Set<Vertex> represents;
     HashSet<Vertex> neighbors;
 
     public Vertex(int id) {
         this.id = id;
         neighbors = new HashSet<>();
+        represents = new HashSet<>();
     }
 
     public void connectTo(Vertex v) {
@@ -30,6 +33,14 @@ public class Vertex {
         if (o == null || getClass() != o.getClass()) return false;
         Vertex vertex = (Vertex) o;
         return id == vertex.id && Objects.equals(neighbors, vertex.neighbors);
+    }
+
+    public int getWeight() {
+        return represents.size();
+    }
+
+    public boolean isRepresentative() {
+        return represents.size() > 0;
     }
 
 }
